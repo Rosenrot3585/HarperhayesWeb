3270585d074d0bccf0d061a0801ad0a86cba3eec
rosenrot3585 <mcmp3585@gmail.com>
2023-01-20 18:45:42 -0300

    dotted background

--- js/email.js
+++ js/email.js
@@ -9,3 +9,13 @@ var submitEmail = document.getElementById("correoElectronico");
 submitEmail.addEventListener("click", () => {
     document.getElementById("contactoViaMail").style.visibility = "visible";
 });
+
+
+/* Fechas
+de Actualización y última visita*/
+
+
+var currentDate = new Date();
+var fullDate = currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear();
+var lastUpdate=document.getElementById("updated-date");
+lastUpdate.textContent=fullDate;
